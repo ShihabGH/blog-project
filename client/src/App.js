8d79eec4fadf211ba81fc97ec8c8a009a305468b
Shihab183 <shihabmahmoodofficial@gmail.com>
2023-06-24 14:19:44 -0500

    footer and navbar files created

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,32 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+  {
+    path: "/write",
+    element: <Write/>,
   },
 ]);
 
