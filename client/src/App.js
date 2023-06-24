48ee63dbcdc5b30adf4ac9cb251189358b4340c7
Shihab183 <shihabmahmoodofficial@gmail.com>
2023-06-24 14:26:03 -0500

    routing optimized for project requirements

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -8,11 +9,37 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Single from "./pages/Single";
 import Write from "./pages/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/post/:id",
+        element: <Single/>
+      },
+      {
+        path: "/write",
+        element: <Write/>
+      },      
+    ]
   },
   {
     path: "/register",
@@ -21,21 +48,15 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login/>,
-  },
-  {
-    path: "/single",
-    element: <Single/>,
-  },
-  {
-    path: "/write",
-    element: <Write/>,
-  },
+  }
 ]);
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
