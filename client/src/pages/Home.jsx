ffe82363b592335e0f36d4a66326955b95593a67
Shihab183 <shihabmahmoodofficial@gmail.com>
2023-08-23 16:44:38 -0500

    Final draft

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,55 +1,52 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
 
-  const posts = [
-    {
-      id: 1,
-      title: "Embracing Elegance and Tradition: The Allure of Japanese Clothing Style",
-      desc: "Japanese clothing style is renowned worldwide for its unique blend of elegance, simplicity, and respect for tradition. Rooted in centuries-old customs and aesthetics, Japanese fashion has captivated the global stage, inspiring designers and fashion enthusiasts alike.",
-      img: "https://images.pexels.com/photos/5266503/pexels-photo-5266503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 2,
-      title: "Sustainable Design: Shaping a Greener Future",
-      desc: "In an era marked by environmental consciousness and the urgent need for sustainable practices, sustainable design has emerged as a powerful tool for creating a greener and more harmonious world. ",
-      img: "https://images.pexels.com/photos/4322027/pexels-photo-4322027.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 3,
-      title: "Unlocking the Power of Coding: A Guide to Learning in 2023",
-      desc: "In the digital age, coding has become an essential skill that opens up a world of opportunities and empowers individuals to shape the future. Whether you aspire to become a software developer, enhance your problem-solving abilities, or simply gain a better understanding of the technology-driven world we live in.",
-      img: "https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 4,
-      title: "Unleashing the Power of E-Sports: A Global Phenomenon",
-      desc: "In the rapidly evolving landscape of digital entertainment, e-sports has emerged as a global phenomenon, captivating millions of fans and redefining the way we perceive competitive gaming.",
-      img: "https://images.pexels.com/photos/7862625/pexels-photo-7862625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-  ];
-  
+  const cat = useLocation().search
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts${cat}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [cat]);
+
+  const getText = (html) =>{
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
+
+
   return (
-    <div className='home'>
-      <div className='posts'>
-        {posts.map(post=>(
-          <div className='post' key={post.id}>
-            <div className='img'>
-              <img src={post.img} alt='postImg'/>
+    <div className="home">
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={`../upload/${post.img}`} alt="" />
             </div>
-            <div className='content'>
-              <Link to={`/post/${post.id}`} className='link'>
+            <div className="content">
+              <Link className="link" to={`/post/${post.id}`}>
                 <h1>{post.title}</h1>
               </Link>
-              <p>{post.desc}</p>
+              <p>{getText(post.desc)}</p>
               <button>Read More</button>
             </div>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,35 +1,81 @@
-import React from 'react';
-import Edit from '../assets/images/edit.png';
-import Delete from '../assets/images/delete.png';
-import {Link} from "react-router-dom";
-import Menu from '../components/Menu';
+import React, { useEffect, useState } from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
+import DOMPurify from "dompurify";
 
 const Single = () => {
-  return (
-    <div className='single'>
-      <div className='single-items'>
-        <div className='content'>
-          <img src='https://images.unsplash.com/photo-1571757767119-68b8dbed8c97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80' alt='gundam' />
-          <div className='user'>
-            <img src='https://images.unsplash.com/photo-1637858868799-7f26a0640eb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80' alt='profileImg'/>
-            <div className='info'>
-              <span>Zombie</span>
-              <p>Posted 2 days ago</p>
-            </div>
-            <div className='edit'>
-              <Link to={`/write?edit=2`}><img className='edit-img' src={Edit} alt=''/></Link>
-              <img src={Delete} alt=''/>
-            </div>
-          </div>
-          <h1>Gundam: A Cultural Icon and Anime Phenomenon</h1>
-          <p className='desc'>Gundam, a franchise that has left an indelible mark on the world of anime, has captured the hearts of fans for over four decades. Since its debut in 1979, this iconic mecha series has become synonymous with the genre, revolutionizing the way people perceive giant robot narratives. This article explores the enduring popularity of Gundam, its impact on anime and pop culture, and the timeless themes it tackles. <br/><br/>
-          A Revolutionary Concept: Gundam, created by Yoshiyuki Tomino, introduced a groundbreaking concept to the mecha genre. Instead of focusing solely on action-packed battles, it delved into intricate political narratives, social commentary, and moral dilemmas. The series explored the human cost of war, the consequences of unchecked technology, and the complexities of personal identity within a futuristic setting. This depth of storytelling and mature themes set Gundam apart from its contemporaries, captivating audiences worldwide. <br/><br/>
-          The Gundam Universe: The Gundam franchise encompasses a vast universe of series, movies, manga, and merchandise. The original Mobile Suit Gundam served as a launching pad, giving birth to multiple timelines and spin-offs, each with its unique storyline, characters, and mecha designs. Notable entries include Gundam Wing, Gundam Seed, Gundam 00, and the recent Iron-Blooded Orphans. With each iteration, Gundam continues to reinvent itself while staying true to its core themes.</p>
-        </div>
-        <Menu/>
-      </div>
-    </div>
-  )
-}
+  const [post, setPost] = useState({});
 
-export default Single
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async ()=>{
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/")
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const getText = (html) =>{
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
+
+  return (
+    <div className="single">
+      <div className="content">
+        <img src={`../upload/${post?.img}`} alt="" />
+        <div className="user">
+          {post.userImg && <img
+            src={post.userImg}
+            alt=""
+          />}
+          <div className="info">
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
+          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
+        </div>
+        <h1>{post.title}</h1>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(post.desc),
+          }}
+        ></p>      </div>
+      <Menu cat={post.cat}/>
+    </div>
+  );
+};
+
+export default Single;
